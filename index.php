b2e5a53000f4663ba6e8a2d535a98ba9a75123f6
Guilherme Ytalo <gydaytalo@gmail.com>
2023-09-15 18:39:04 -0300

    fix: file format

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 </head>
 
 <body>
+    <div>
     <div class="flex flex-col justify-center items-center h-screen">
         <div class="text-center">
             <h1 class="text-2xl font-semibold">Cadastro de Mídia</h1>
         </div>
-        <form class="mt-4 w-full max-w-md">
+        <form id="cadastroForm" class="mt-4 w-full max-w-md">
             <div class="mb-4">
                 <label for="tipo" class="block text-gray-700">Tipo (CD, DVD, Vinil, K7, etc.):</label>
                 <input type="text" name="tipo" required class="mt-1 p-2 w-full border rounded">
@@ -48,6 +49,46 @@
             </div>
         </form>
     </div>
+    <div>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // Recupera os dados do formulário
+                $tipo = $_POST["tipo"];
+                $interprete = $_POST["interprete"];
+                $album = $_POST["album"];
+                $data_compra = $_POST["data_compra"];
+                $valor_pago = $_POST["valor_pago"];
+                $observacao = $_POST["observacao"];
+        
+                // Exibe os dados preenchidos
+                echo "<p><strong>Tipo:</strong> $tipo</p>";
+                echo "<p><strong>Intérprete:</strong> $interprete</p>";
+                echo "<p><strong>Álbum:</strong> $album</p>";
+                echo "<p><strong>Data da Compra:</strong> $data_compra</p>";
+                echo "<p><strong>Valor Pago:</strong> R$ $valor_pago</p>";
+                echo "<p><strong>Observação:</strong> $observacao</p>";
+            } else {
+                // Se o formulário não foi enviado, exibe uma mensagem de erro
+                echo "<p>Erro: O formulário não foi enviado.</p>";
+            }
+        ?>
+    </div>
+    </div>
+
+    <script>
+        document.getElementById("cadastroForm").addEventListener("submit", function (event) {
+            const tipo = document.getElementsByName("tipo")[0].value;
+            const interprete = document.getElementsByName("interprete")[0].value;
+            const album = document.getElementsByName("album")[0].value;
+            const dataCompra = document.getElementsByName("data_compra")[0].value;
+            const valorPago = document.getElementsByName("valor_pago")[0].value;
+
+            if (!tipo || !interprete || !album || !dataCompra || !valorPago) {
+                alert("Por favor, preencha todos os campos obrigatórios.");
+                event.preventDefault(); // Impede o envio do formulário
+            }
+        });
+    </script>
 </body>
 
 </html>
